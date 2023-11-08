e595f3951d9a3e1737bca56ac9c75f6c8f9e741b
Percy <percy@railway.app>
2023-11-08 14:00:38 +0000

    Initial Commit

---
+++ common/const.py
@@ -0,0 +1,15 @@
+# bot_type
+OPEN_AI = "openAI"
+CHATGPT = "chatGPT"
+BAIDU = "baidu"
+XUNFEI = "xunfei"
+CHATGPTONAZURE = "chatGPTOnAzure"
+LINKAI = "linkai"
+CLAUDEAI = "claude"
+
+MODEL_LIST = ["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "wenxin", "wenxin-4", "xunfei", "claude"]
+
+# model
+GPT4 = "gpt-4"
+GPT4_PREVIEW = "gpt-4-1106-preview"
+GPT4_VISION_PREVIEW = "gpt-4-vision-preview"
